a4f6867b5ea8bffee79f9767df3e79c4a85c7d45
Atharva Deosthale <atharva.deosthale17@gmail.com>
2024-03-06 02:09:41 +0530

    server: remove cors stuff, socket.io handling it already

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -4,18 +4,8 @@ import { setupListeners } from "./setupListeners";
 
 const PORT = process.env.PORT || 8080;
 
-const httpServer = createServer((req, res) => {
-  // Set CORS headers
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Request-Method", "*");
-  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  if (req.method === "OPTIONS") {
-    res.writeHead(200);
-    res.end();
-    return;
-  }
-});
+const httpServer = createServer();
+
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
